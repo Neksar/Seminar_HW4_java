618a2d865a1732c039cc684ebead3bc6fbaca77c
Maksim Bondarenko <m.bondarenko1988@yandex.ru>
2023-06-13 20:01:56 +0300

    Done homework to four seminar about java

---
+++ UserGroup.java
@@ -0,0 +1,29 @@
+import java.util.List;
+
+public class UserGroup implements Iterable<User>{
+    private List<User> users;
+
+    public UserGroup(List<User> users) {
+        this.users = users;
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+
+    @Override
+    public String toString() {
+        return "UserGroup{" +
+               "users=" + users +
+               '}';
+    }
+
+    @Override
+    public UserGroupIterator iterator() {
+        return new UserGroupIterator(this);
+    }
+}
